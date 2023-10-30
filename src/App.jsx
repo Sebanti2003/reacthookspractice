21e9da1902c3c52babd6395b6ba6f0e74c7eaf76
Sebanti2003 <dasguptasebanti2003@gmail.com>
2023-10-30 19:14:28 +0530

    commited the whole setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    {/* We are wrapped up in a react fragmant */}
+    
+    </>
+  )
+}
+
+export default App
